fa41a072400d3e96a226d134cf829bf6f8f89fa5
Seoa Kim <seoa521@gmail.com>
2023-07-11 18:52:36 +0900

    day_4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, {useState} from "react";
+import React, { useState, useCallback } from "react";
 import "./App.css";
 import Lists from "./components/Lists";
 import Form from "./components/Form";
 
 export default function App() {
-
   const [todoData, setTodoData] = useState([]);
   const [value, setValue] = useState("");
 
@@ -16,27 +15,31 @@ export default function App() {
     let newTodo = {
       id: Date.now(),
       title: value,
-      completed: false
+      completed: false,
     };
 
     // 원래 있던 할 일에 새로운 할 일 더해주기
     // setter에서 이전 state를 가지고 오기 위해서는 인수에 함수를 이용해서 사용할 수 있음
-    setTodoData(prev => [...prev, newTodo]);
+    setTodoData((prev) => [...prev, newTodo]);
     setValue("");
   };
-  
 
-    return(
-      <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
-        <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
-          <div className="flex justify-between mb-3">
-            <h1>할 일 목록</h1>
-          </div>
+  // 할 일 리스트 모두 지우기 버튼 생성
+  const handleRemoveClick = () => {
+    setTodoData([]);
+  };
 
-          <Lists todoData={todoData} setTodoData={setTodoData}/>
-          <Form handleSubmit={handleSubmit} value={value} setValue={setValue}/>
-          
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+      <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
+        <div className="flex justify-between mb-3">
+          <h1>할 일 목록</h1>
+          <button onClick={handleRemoveClick}>Delete All</button>
         </div>
+
+        <Lists todoData={todoData} setTodoData={setTodoData} />
+        <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
       </div>
-    )
+    </div>
+  );
 }
--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,42 +1,59 @@
-import React from 'react'
+import { prettyDOM } from "@testing-library/react"; // eslint-disable-line no-unused-vars
+import React from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import List from "./List";
 
-export default function Lists({todoData, setTodoData}) {
+const Lists = React.memo(({ todoData, setTodoData }) => {
+  const handleEnd = (result) => {
+    // result 매개변수에는 source 항목 및 대상 위치와 같은 드래그 이벤트에 대한 정보가 포함됨
+    console.log("result", result);
+    // 목적지가 없으면(이벤트 취소) 이 함수를 종료함
+    if (!result.destination) return;
 
-  const handleCompleteChange = (id) => {
-    let newTodoData = todoData.map(data => {
-      if(data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    })
+    // 리액트 불변성을 지켜주기 위해 새로운 todoData 생성
+    const newTodoData = [...todoData];
+
+    // 1. 변경시키는 아이템을 배열에서 지워줌
+    // 2. return 값으로 지워진 아이템을 잡아줌
+    const [reorderedItem] = newTodoData.splice(result.source.index, 1);
+
+    // 원하는 자리에 reorderItem을 insert 해줌
+    newTodoData.splice(result.destination.index, 0, reorderedItem);
     setTodoData(newTodoData);
   };
-
-  const handleClick = (id) => {
-    let newTodoData = todoData.filter((data) => data.id !== id);
-    console.log('newTodoData', newTodoData);
-    setTodoData(newTodoData);
-  };
-
   return (
     <div>
-      {todoData.map((data) => (
-        <div key={data.id}>
-          <div className="flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded">
-            <div className="items-center">
-              <input 
-                type="checkbox"  
-                onChange={() => handleCompleteChange(data.id)}
-                defaultChecked={data.completed}
-              />{" "}
-              <span className={data.completed ? "line-through" : undefined}>{data.title}</span>
+      <DragDropContext onDragEnd={handleEnd}>
+        <Droppable droppableId="todo">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {todoData.map((data, index) => (
+                <Draggable
+                  key={data.id}
+                  draggableId={data.id.toString()}
+                  index={index}
+                >
+                  {(provided, snapshot) => (
+                    <List
+                      key={data.id}
+                      id={data.id}
+                      title={data.title}
+                      completed={data.completed}
+                      todoData={todoData}
+                      setTodoData={setTodoData}
+                      provided={provided}
+                      snapshot={snapshot}
+                    />
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
             </div>
-            <div>
-              <button className="px-4 py-2 float-right" onClick={() => handleClick(data.id)}>x</button>
-            </div>
-          </div>
-        </div>
-      ))}
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
-}
+});
+
+export default Lists;
